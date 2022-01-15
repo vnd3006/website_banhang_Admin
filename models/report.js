ce38d9303406a5de42ac99ef7df174db75619d5e
Duy Võ <55312044+vnd3006@users.noreply.github.com>
2022-01-15 15:06:52 +0700

    top10

--- models/report.js
+++ models/report.js
@@ -2,13 +2,13 @@ const ObjectId = require('mongodb').ObjectId;
 const { dbs } = require('../dbs');
 
 const detail = async (id) => {
-    const results = await dbs.production.collection('orders').find({_id: ObjectId(id)})
+    const results = await dbs.production.collection('order').find({_id: ObjectId(id)})
       .toArray();
     return results[0];
 };
 exports.detail = detail;
 
 module.exports.list = async () => {
-    return results = await dbs.production.collection('orders').find({trangThai:true})
+    return results = await dbs.production.collection('product').find().sort({soldCount: -1})
         .toArray();
 };
